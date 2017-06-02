d50e1cdc090389fd5c4add5d72e5ce54d0467406
Alice <aliceli.gm@gmail.com>
2017-06-02 13:57:50 -0500

    uncomment line 45 for to fix error

--- run_server.sh
+++ run_server.sh
@@ -42,7 +42,7 @@ if [[ "${TEAM_ID}" == "" || "${TEAM_SECRET}" == "" || "${PORT}" == "" || "${PERS
 fi
 
 
-#cd './bin'
+cd './bin'
 if [ "${RELAY_ADDRESS}" == "" ] ; then
   java -classpath './third_party/mysql-connector-java-5.1.42-bin.jar;./bin' codeu.chat.ServerMain \
       "${TEAM_ID}" \
